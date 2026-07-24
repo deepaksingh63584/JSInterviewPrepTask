1f92ff647957ed66c7faa98ce7311e3706820566
deepaksingh63584 <thakursingh63584@gmail.com>
2026-07-25 01:15:53 +0530

    Dublicate string from sentance

---
+++ InterviewTask/String/duplicateStringFromSentance.js
@@ -0,0 +1,26 @@
+let str = "this is a test this is only a test";
+
+let words = str.split(" ");
+let result = "";
+
+for (let i = 0; i < words.length; i++) {
+    let isDuplicate = false;
+
+    // Check if the current word has already appeared
+    for (let j = 0; j < i; j++) {
+        if (words[i] === words[j]) {
+            isDuplicate = true;
+            break;
+        }
+    }
+
+    if (!isDuplicate) {
+        if (result === "") {
+            result = words[i];
+        } else {
+            result = result + " " + words[i];
+        }
+    }
+}
+
+console.log(result);
