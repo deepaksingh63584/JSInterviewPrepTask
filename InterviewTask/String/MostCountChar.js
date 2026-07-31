321447226494375237a0d28715fe6651dcafe8ca
deepaksingh63584 <thakursingh63584@gmail.com>
2026-07-31 15:57:06 +0530

    most count chat in string

---
+++ InterviewTask/String/MostCountChar.js
@@ -0,0 +1,20 @@
+// Find Most Frequent Item in Array - Multiple Approaches
+const arr1 = "ksfhsdiufhskdfgggggggggjdfhgbksm";
+
+// Method 1: Using Object/Map to count frequencies
+function mostFrequent1(arr) {
+    const freq = {};
+    let maxCount = 0;
+    let maxItem = "";
+    for (let item of arr) {
+        freq[item] = (freq[item] || 0) + 1;
+        if (freq[item] > maxCount) {
+            maxCount = freq[item];
+            maxItem = item;
+        }
+    }
+    return { maxItem, maxCount };
+}
+console.log("Original array:", arr1);
+
+console.log("Method 1 :", mostFrequent1(arr1));
